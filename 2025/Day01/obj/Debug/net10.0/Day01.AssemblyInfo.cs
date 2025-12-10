81947c8380b51ba197e4a1da41c1bca214cd6dfc
Tygo Geervliet <tygo@tygodev.com>
2025-12-10 19:38:13 +0100

    fixed folder structure Day02

--- 2025/Day01/obj/Debug/net10.0/Day01.AssemblyInfo.cs
+++ 2025/Day01/obj/Debug/net10.0/Day01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f52ea1346b091e77da2f18b2025a28b9a9ae9192")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0121dbf022b3021dd1ada816f9f32ed67e5a5ee0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
