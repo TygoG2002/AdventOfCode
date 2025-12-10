78d604f3cfc45f5c974963a8dfa2aec31712fb3c
Tygo Geervliet <tygo@tygodev.com>
2025-12-10 19:29:17 +0100

    Initial Advent of Code setup

---
+++ 2025/Day01/Program.cs
@@ -0,0 +1,29 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+
+int start = 50;
+
+int GetPassword(int start)
+{
+    int count = 0;
+
+    foreach (string line in lines)
+    {
+        char direction = line[0];
+        int amount = int.Parse(line[1..]);
+
+        if (direction == 'L')
+            start -= amount;
+        else
+            start += amount;
+
+        // stay between 0–99
+        start = (start % 100 + 100) % 100;
+
+        if (start == 0)
+            count++;
+    }
+
+    return count;
+}
+
+Console.WriteLine(GetPassword(start));
