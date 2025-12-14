507eca3ea568a120bf98a8ebeb053c51b86230dd
Tygo <tygo@tygodev.com>
2025-12-14 11:48:55 +0100

    Improve performance by generating repeated digit IDs instead of linear scan

--- 2025/Day02/Program.cs
+++ 2025/Day02/Program.cs
@@ -1,56 +1,37 @@
-﻿using System.Runtime.CompilerServices;
-using Day02;
+﻿using Day02;
 
-string input = File.ReadAllText("input.txt");
-
-
-
-List<string> myIDList = input.SplitBasedOnSeparator(',');
-Dictionary<long, long> invalidIDs = new Dictionary<long, long>();
+string input = File.ReadAllText("input.txt").Trim();
 long sum = 0;
 
-foreach (string ID in myIDList)
+foreach (var range in input.SplitBasedOnSeparator(','))
 {
-    List<string> test = ID.SplitBasedOnSeparator('-');
+    var parts = range.SplitBasedOnSeparator('-');
 
+    long start = long.Parse(parts[0]);
+    long end = long.Parse(parts[1]);
 
-    if (!long.TryParse(test[0], out long startSearchRange) ||
-        !long.TryParse(test[1], out long endSearchRange))
+    int maxDigits = end.ToString().Length;
+
+    for (int fullDigits = 2; fullDigits <= maxDigits; fullDigits += 2)
     {
-        throw new ArgumentException("Invalid range values");
+        int halfDigits = fullDigits / 2;
+
+        long minHalf = (long)Math.Pow(10, halfDigits - 1);
+        long maxHalf = (long)Math.Pow(10, halfDigits) - 1;
+
+        for (long half = minHalf; half <= maxHalf; half++)
+        {
+            long full = long.Parse(half.ToString() + half.ToString());
+
+            if (full < start)
+                continue;
+
+            if (full > end)
+                break;
+
+            sum += full;
+        }
     }
-
-
-
-
-    for (long i = startSearchRange; i <= endSearchRange; i++)
-    {
-        if (i.IsInvalidId())
-            sum += i;
-    }
-
-
-
-
-
-
-    {
-    }
-
-
 }
+
 Console.WriteLine(sum);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
