72b2402a3134c2e4c3e8810c59aa379eb6f4ee06
Tygo <tygo@tygodev.com>
2025-12-14 18:41:00 +0100

    Completed first part of puzzle. not clean solution. will make it better

--- 2025/Day04/Program.cs
+++ 2025/Day04/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] lines = File.ReadAllLines("input.txt");
+
+int totalNumberOfPaperRolls = 0;
+
+for(int i = 0; i < lines.Length; i++){
+
+    string currentLine = lines[i];
+    string? previousLine = i > 0 ? lines[i - 1] : null;
+    string? nextLine = i < lines.Length - 1 ? lines[i + 1] : null;
+
+    int currentLineIndex = i;
+
+    for (int j = 0; j< currentLine.Length; j++)
+    {
+        int neighborCount = 0;
+
+        if (currentLine[j] != '@')
+            continue;
+
+
+        int indexOfCurrentValue = j;
+
+        bool left = indexOfCurrentValue != 0 && currentLine[j - 1] == '@';
+        bool right = indexOfCurrentValue != (currentLine.Length-1) && currentLine[j + 1] == '@';
+        bool top = previousLine != null && previousLine[j] == '@';
+        bool bottom = nextLine != null && nextLine[indexOfCurrentValue] == '@';
+        bool currentValue = currentLine[j] == '@';
+
+        bool topLeft = previousLine != null && indexOfCurrentValue != 0 && previousLine[j - 1] == '@';
+        bool topRight = previousLine != null && indexOfCurrentValue != (currentLine.Length - 1) && previousLine[j + 1] == '@';
+        bool bottomLeft = nextLine != null && indexOfCurrentValue != 0 && nextLine[j - 1] == '@';
+        bool bottomRight = nextLine != null && indexOfCurrentValue != (currentLine.Length - 1) && nextLine[j + 1] == '@';
+
+        if (left) neighborCount++;
+        if (right) neighborCount++;
+        if (top) neighborCount++;
+        if (bottom) neighborCount++;
+        if (topLeft) neighborCount++;
+        if (topRight) neighborCount++;
+        if (bottomLeft) neighborCount++;
+        if (bottomRight) neighborCount++;
+
+        if (neighborCount < 4)
+        {
+            totalNumberOfPaperRolls++;
+        }
+
+    }
+}
+
+Console.WriteLine(totalNumberOfPaperRolls);
