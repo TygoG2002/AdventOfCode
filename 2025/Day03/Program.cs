f5f08c4a8332be474839f31b312bb0cec1f96b1d
Tygo <tygo@tygodev.com>
2025-12-14 16:23:45 +0100

    Solved first part puzzle

--- 2025/Day03/Program.cs
+++ 2025/Day03/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] lines = File.ReadAllLines("input.txt");
+
+int totalSum = 0;
+
+foreach (string line in lines)
+{
+    int maxForThisLine = 0;
+
+    for (int i = 0; i < line.Length; i++)
+    {
+        for (int j = i + 1; j < line.Length; j++)
+        {
+            int firstDigit = int.Parse(line[i].ToString());
+            int secondDigit = int.Parse(line[j].ToString());
+            int value = firstDigit * 10 + secondDigit;
+
+            maxForThisLine = Math.Max(maxForThisLine, value);
+        }
+    }
+
+    totalSum += maxForThisLine;
+}
+
+Console.WriteLine(totalSum);
